596538e9cc77fae225b6e66c4d5aa57c3f77387d
darren <764190362@qq.com>
2017-10-23 10:10:26 +0800

    提交表单

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@
                     
                     <div class="row">
                         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                            <button class="btn btn-primary">
+                            <button class="btn btn-primary" @click.prevent="submitted">
                                 Submit
                             </button>
                         </div>
@@ -101,7 +101,7 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -141,7 +141,13 @@
                 gender: 'Male',
                 selectedPriority: 'High',
                 priorities: ['High', 'Medium', 'Low'],
-                dataSwitch: true
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted(){
+                this.isSubmitted = true;
             }
         },
         components: {
