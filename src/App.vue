1b4b08dacd7e76cb9cdf1b44afbb085e0533d4e9
darren <764190362@qq.com>
2017-10-23 10:05:30 +0800

    自定义一个Input组件也使用v-model,其内部其实是通过事件改变props中的value属性值

--- src/App.vue
+++ src/App.vue
@@ -83,6 +83,20 @@
                         </div>
                     </div>
                     
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                            <app-switch v-model="dataSwitch"></app-switch>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                            <button class="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </form>
@@ -104,6 +118,7 @@
                         </ul>
                         <p>Gender:{{gender}}</p>
                         <p>Priority:{{selectedPriority}}</p>
+                        <p>Switch: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -112,6 +127,7 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
     export default {
         data() {
             return {
@@ -124,8 +140,12 @@
                 sendMail: [],
                 gender: 'Male',
                 selectedPriority: 'High',
-                priorities: ['High', 'Medium', 'Low']
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true
             }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
